a9f9d5eaa87233355ef60b0511349c434315ce98
tadeo alvarez <tadeo.regalado.alvarez@gmail.com>
2024-03-30 22:39:52 -0600

    unit test for loanMapper

--- crud/src/main/java/com/app/crud/dto/mapper/LoanDTOMapper.java
+++ crud/src/main/java/com/app/crud/dto/mapper/LoanDTOMapper.java
@@ -11,21 +11,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoanDTOMapper {
     public LoanDTO mapToLoanDTO(Loan loan) {
+        if(loan == null) {
+            throw new NullPointerException("Loan is null");
+        }
         Member member = loan.getMember();
-
-
+        if(member == null) {
+            throw new NullPointerException("Member is null");
+        }
         MemberDTO memberDTO = new MemberDTO(
                 member.getMemberId(),
                 member.getEmail(),
-                member.getPassword(),
                 member.getUsername(),
+                member.getPassword(),
                 member.getName(),
                 member.getLastname(),
                 member.getAge(),
                 member.getRoles(),
                 member.getPermissions()
         );
-
         return new LoanDTO(
                 loan.getID(),  // Changed from loan.getID() to loan.getId()
                 loan.getDateLimit(),
@@ -35,12 +38,18 @@ public class LoanDTOMapper {
     }
 
     public Loan mapToLoan(LoanDTO loanDTO) {
+        if(loanDTO == null) {
+            throw new NullPointerException("LoanDTO is null");
+        }
         MemberDTO memberDTO = loanDTO.getMemberDTO();
+        if(memberDTO == null) {
+            throw new NullPointerException("MemberDTO attribute is null");
+        }
         Member member = new Member(
                 memberDTO.getMemberId(),
                 memberDTO.getEmail(),
-                memberDTO.getPassword(),
                 memberDTO.getUsername(),
+                memberDTO.getPassword(),
                 memberDTO.getName(),
                 memberDTO.getLastname(),
                 memberDTO.getAge(),
