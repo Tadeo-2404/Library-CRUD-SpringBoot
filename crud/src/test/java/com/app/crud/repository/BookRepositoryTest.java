bf59424fe2f85eddbf34d69a5961dafd61c9a240
tadeo alvarez <tadeo.regalado.alvarez@gmail.com>
2024-03-31 19:29:25 -0600

    first repo unit test

---
+++ crud/src/test/java/com/app/crud/repository/BookRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.app.crud.repository;
+
+import com.app.crud.model.book.Book;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+@DataJpaTest
+public class BookRepositoryTest {
+    @Autowired
+    BookRepository bookRepository;
+    Book book;
+
+    @BeforeEach
+    public void setup() {
+        book = new Book("ISBN", "title", "author", "genre", 10);
+        this.bookRepository.save(book);
+    }
+
+    @Test
+    public void findByTitleFound() {
+        List<Book> bookList = this.bookRepository.findByTitle("title");
+        Assertions.assertEquals(bookList.get(0).getTitle(), this.book.getTitle(), "Title not found");
+    }
+
+    @AfterEach
+    public void teardown() {
+        this.book = null;
+        this.bookRepository.deleteAll();;
+    }
+}
